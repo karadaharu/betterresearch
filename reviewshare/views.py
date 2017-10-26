f02d03e6db7624420cc1381472041b7fe7cc005c
Kazumasa <catter121@gmail.com>
2017-10-27 04:08:43 +0900

    add template

--- reviewshare/views.py
+++ reviewshare/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world")
+    template = loader.get_template('reviewshare/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))

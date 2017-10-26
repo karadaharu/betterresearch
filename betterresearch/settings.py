c23515d9eac495c8e2323400ea3db97643b99557
Kazumasa <catter121@gmail.com>
2017-10-27 04:01:24 +0900

    create model

--- betterresearch/settings.py
+++ betterresearch/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'reviewshare.apps.ReviewshareConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- reviewshare/models.py
+++ reviewshare/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Paper(models.Model):
+    doi = models.CharField(max_length=200)
+    title = models.CharField(max_length=500)
+
+class Review(models.Model):
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+    url = models.CharField(max_length=300)

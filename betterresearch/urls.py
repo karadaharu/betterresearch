368d305a8b28510f60098ed1d53932767af24dd4
Kazumasa <catter121@gmail.com>
2017-11-08 18:46:23 +0100

    remove url hardcode

--- betterresearch/urls.py
+++ betterresearch/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^reviewshare/', include('reviewshare.urls')),
+    url(r'^', include('reviewshare.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
